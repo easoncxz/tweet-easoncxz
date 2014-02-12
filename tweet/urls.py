185ee49a3a32a7db1171b8fd34eea43f3086571a
Eason Chen <me@easoncxz.com>
2014-02-12 23:36:27 +1300

    Removed the bloat 'home' view.

--- tweet/urls.py
+++ tweet/urls.py
@@ -1,10 +1,9 @@
 from django.conf.urls import patterns, url
-
 from tweet import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^auth/$', views.auth, name='auth'),
     url(r'^callback/$', views.callback, name='callback'),
-    url(r'^home/$', views.home, name='home'),
+    #url(r'^home/$', views.home, name='home'),
 )
--- tweet/views.py
+++ tweet/views.py
@@ -11,18 +11,33 @@ from constants import service_name, consumer_key, consumer_secret, request_token
 from tweet.models import OneRequestToken, OneAccessToken
 
 def index(request):
-    access_token = ''
     if 'access_token' in request.COOKIES:
-        access_token = request.COOKIES.get('access_token')
-        return HttpResponseRedirect(reverse('tweet:home') + "?access_token=" + access_token)
+        # Try to resume logged-in state - by showing the homepage.
+        at = request.COOKIES.get('access_token')
+        try:
+            t = OneAccessToken.objects.get(access_token=at)
+        # except non-1 number of objects returned - 
+        #   the user passed a crap cookie, or we recorded an AT twice.
+        finally:
+            pass
+        ats = t.access_token_secret
+        twitter = _get_twitter()
+        session = twitter.get_session((at, ats))
+        r = session.get('account/verify_credentials.json')
+        from pprint import pformat
+        import json
+        j = json.loads(r.content)
+        response_text = pformat(j)
+        return HttpResponse('<pre>' + response_text + '</pre>')
     else:
+        # user not logged in.
         context = RequestContext(
             request,{'authorize_url': reverse('tweet:auth')})
         template = loader.get_template('tweet/index.html')
         return HttpResponse(template.render(context))
 
 def auth(request):
-    # work out an authorization url and point the user there
+    """Ask Twitter for an authorization url and redirect the user there."""
     twitter = _get_twitter()
     rt, rts = twitter.get_request_token() 
     if len(OneRequestToken.objects.filter(request_token=rt)) == 0:
@@ -36,6 +51,7 @@ def auth(request):
     return HttpResponseRedirect(twitter.get_authorize_url(rt))
 
 def callback(request):
+    """Handles control when Twitter passes it back after the user has authorized us."""
     # get stuff twitter passed back via the user
     try:
         oauth_token = request.GET['oauth_token']
@@ -59,6 +75,8 @@ def callback(request):
             'oauth_verifier': oauth_verifier,
             'callback_url': 'http://tweet-easoncxz.herokuapp.com/callback',
         })
+
+    # see if we already have the same access token
     if len(OneAccessToken.objects.filter(access_token=at)) == 0:
         OneAccessToken(
             access_token=at,
@@ -67,27 +85,9 @@ def callback(request):
         t = OneAccessToken.objects.get(access_token=at)
         t.access_token_secret = ats
         t.save()
-    response = HttpResponseRedirect(reverse('tweet:home') + "?access_token=" + at)
+    response = HttpResponseRedirect(reverse('tweet:index'))
     response.set_cookie('access_token', at)
     return response
-
-def home(request):
-    if ('access_token' not in request.GET) or (request.GET['access_token'] not in [t.access_token for t in OneAccessToken.objects.filter(access_token=request.GET['access_token'])]):
-        # Should remove that cookie now
-        return HttpResponseRedirect(reverse('tweet:index'))
-    else:
-        at =  request.GET['access_token']
-        t = OneAccessToken.objects.get(access_token=at)
-        ats = t.access_token_secret
-        twitter = _get_twitter()
-        session = twitter.get_session((at, ats))
-        r = session.get('account/verify_credentials.json')
-        assert r.status_code == 200, 'twitter didn\'t give stuff back properly'
-        from pprint import pformat
-        import json
-        j = json.loads(r.content)
-        response_text = pformat(j)
-        return HttpResponse('<pre>' + response_text + '</pre>')
 
 def _get_twitter():
     return OAuth1Service(
